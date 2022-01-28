c8b9f7208f76bcf2032e321befc67d7e6c8ebb12
Aung Wai Yan Htun <htunwaiyan817@gmail.com>
2022-01-28 17:23:25 +0630

    my project

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// START TASK SECTION 
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+// End TASK SECTION 
+
+// START FOOTER 
+const getyear = document.getElementById('getyear');
+const getfullyear = new Date().getFullYear();
+getyear.textContent=getfullyear;
+// END FOOTER
